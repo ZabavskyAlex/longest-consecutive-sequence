024946438aa3353be13a51bf9bb6f1dda4a1e5de
zabavsky.alex <zabavsky.alex@mail.ru>
2018-02-23 16:34:40 +0300

    longest-consecutive-sequence

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 module.exports = function longestConsecutiveLength(array) {
    let main_lon = 0;
    let dict = {};
-   let a = array;
    for (let i = 0; i < array.length; i++){
        dict[array[i]] = true;
    }
@@ -10,8 +9,7 @@ module.exports = function longestConsecutiveLength(array) {
      let buf_lon = 1;
      let i = 1;
      while (flag){
-       if(parseInt(key) + i in dict){
-           let kkk = key;
+       if(dict[parseInt(key)+i]){
            buf_lon++;
            i++;
        }
@@ -27,10 +25,7 @@ module.exports = function longestConsecutiveLength(array) {
    array.sort(function (a, b) {
       return a - b;
    });
-   //let a = [array[636], array[637],array[638], array[639], array[640], array[641], array[642]];
-
-   // console.log(array);
-   if(array.length == 1){
+    if(array.length == 1){
       main_lon = buf_lon;
    }
    else {
@@ -38,11 +33,9 @@ module.exports = function longestConsecutiveLength(array) {
            let a = array[i];
            let b = array[i+1];
            if(array[i] == array[i+1] - 1){
-
                buf_lon++;
                if(buf_lon > main_lon){
                    main_lon = buf_lon;
-
                }
            } else buf_lon = 1;
        }
